9fc6a0512ccd8f9dfffd66e580bebd1da66b4c0e
masashikuwahara <brucemclarenp4@gmail.com>
2024-05-22 21:56:09 +0900

    Result.vue削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Select from '../views/Select.vue';
-import Result from '../views/Result.vue';
 
 const routes = [
   {
@@ -13,11 +12,6 @@ const routes = [
     path: '/select',
     name: 'Select',
     component: Select
-  },
-  {
-    path: '/result',
-    name: 'Result',
-    component: Result
   }
 ];
 
--- src/views/Result.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>ここは結果のページです</h1>
-  </div>
-</template>
